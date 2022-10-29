adecbbb2ed78403cb79fbb8d98835354f645ab8f
Muhammad Syaifudin <syaifudinm84@gmail.com>
2022-10-29 22:47:11 +0700

    slicing custom widget part 1

--- lib/ui/pages/onboarding_page.dart
+++ lib/ui/pages/onboarding_page.dart
@@ -1,5 +1,6 @@
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/pages/sign_in_page.dart';
+import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
@@ -100,27 +101,9 @@ class _OnboardingPageState extends State<OnboardingPage> {
                     currentIndex == 2
                         ? Column(
                             children: [
-                              SizedBox(
-                                width: double.infinity,
-                                height: currentIndex == 2 ? 38 : 50,
-                                child: TextButton(
-                                  onPressed: () {
-                                    carouselController.nextPage();
-                                  },
-                                  style: TextButton.styleFrom(
-                                    backgroundColor: purpleColor,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(56),
-                                    ),
-                                  ),
-                                  child: Text(
-                                    'Get Started',
-                                    style: whiteTextStyle.copyWith(
-                                      fontSize: 16,
-                                      fontWeight: semiBold,
-                                    ),
-                                  ),
-                                ),
+                              CustomFilledButton(
+                                title: 'Get Started',
+                                onPressed: () {},
                               ),
                               const SizedBox(
                                 height: 20,

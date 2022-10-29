e82a416c30f93265cc209d4bfe8d78a023aaa22a
Muhammad Syaifudin <syaifudinm84@gmail.com>
2022-10-29 22:29:36 +0700

    slicing sign in part 2

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -45,11 +45,103 @@ class SignInPage extends StatelessWidget {
               color: whiteColor,
             ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // NOTE: EMAIL INPUT
+                //NOTE: EMAIL INPUT
+                Text(
+                  'Email Addres',
+                  style: blackTextStyle.copyWith(
+                    fontWeight: medium,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                    contentPadding: const EdgeInsets.all(12),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                //NOTE: PASSWORD INPUT
+                Text(
+                  'Password',
+                  style: blackTextStyle.copyWith(
+                    fontWeight: medium,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                    contentPadding: const EdgeInsets.all(12),
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'Forgot Password',
+                    style: blueTextStyle,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: purpleColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(56),
+                      ),
+                    ),
+                    child: Text(
+                      'Sign In',
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
-          )
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 24,
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+              ),
+              child: Text(
+                'Create New Account',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );

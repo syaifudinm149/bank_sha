c40748de9c19fa94f09036cf059dda79c060964b
Muhammad Syaifudin <syaifudinm84@gmail.com>
2022-11-10 22:14:10 +0700

    slicing page floating action button

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,51 +8,70 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBackgroundColor,
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: blueColor,
-        unselectedItemColor: blackColor,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedLabelStyle: blueTextStyle.copyWith(
-          fontSize: 10,
-          fontWeight: medium,
+      bottomNavigationBar: BottomAppBar(
+        color: whiteColor,
+        shape: const CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6,
+        elevation: 0,
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: whiteColor,
+          elevation: 0,
+          selectedItemColor: blueColor,
+          unselectedItemColor: blackColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedLabelStyle: blueTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          unselectedLabelStyle: blackTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_overview.png',
+                width: 20,
+                color: blueColor,
+              ),
+              label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_history.png',
+                width: 20,
+              ),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_statistic.png',
+                width: 20,
+              ),
+              label: 'Statistic',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_reward.png',
+                width: 20,
+              ),
+              label: 'Reward',
+            ),
+          ],
         ),
-        unselectedLabelStyle: blackTextStyle.copyWith(
-          fontSize: 10,
-          fontWeight: medium,
-        ),
-        items: [
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_overview.png',
-              width: 20,
-              color: blueColor,
-            ),
-            label: 'Overview',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_history.png',
-              width: 20,
-            ),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_statistic.png',
-              width: 20,
-            ),
-            label: 'Statistic',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_reward.png',
-              width: 20,
-            ),
-            label: 'Reward',
-          ),
-        ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: purpleColor,
+        child: Image.asset(
+          'assets/ic_plus_circle.png',
+          width: 24,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
